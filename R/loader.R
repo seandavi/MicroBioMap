d5a49d9c777962ca81c0c472538a65756d79edaa
Sean Davis <seandavi@gmail.com>
2023-07-26 10:17:07 -0400

    loader and DESCRIPTION

--- R/loader.R
+++ R/loader.R
@@ -1,3 +1,6 @@
+library(TreeSummarizedExperiment)
+library(ape)
+
 #'
 #' @export
 #'
@@ -35,4 +38,62 @@ loadme <- function(fname) {
     assays=list(counts=t(mat))
   )
   td
+}
+
+taxonname2edgelist <- function(taxon) {
+  print(taxon)
+  v = strsplit(taxon,'\\.')[[1]]
+  v = v[!v=='NA']
+  if(length(v)>1) {
+    lv = length(v)
+    df = data.frame(from=v[1:(lv-1)], to = v[2:lv])
+  } else {
+    df = data.frame()
+  }
+  df
+}
+
+taxa2edgelist <- function(taxa) {
+  taxa_edgelist <- lapply(taxa,taxonname2edgelist)
+  df = unique(do.call(rbind, taxa_edgelist))
+  return(df)
+  unique_names = unique(c(df$from,df$to))
+  l = seq_along(unique_names)
+  names(l) = unique_names
+  parents = l[df$from]
+  nodes = l[df$to]
+  df$parent=parents
+  df$node = nodes
+  df$label = df$to
+  df
+}
+
+taxa2phylo <- function(taxa) {
+  edgelist = taxa2edgelist(taxa)
+  edgelist = as.matrix(edgelist)
+
+  edgelist = edgelist[!is.na(edgelist[,1]) & !is.na(edgelist[,2]),]
+
+  from <- edgelist[,1]
+  to <- edgelist[,2]
+  ids <- unique(c(edgelist[,1], edgelist[,2]))
+
+  tip.label <- setdiff(ids, from)
+  node.label <- unique(from)
+
+  # make a map from taxonomy ID to internal 1:n ids
+  idmap <- 1:(length(tip.label) + length(node.label))
+  names(idmap) <- c(tip.label, node.label)
+
+  # make a phylo object
+  tree <- list(
+    edge       = matrix(c(idmap[as.character(from)], idmap[as.character(to)]), ncol=2),
+    tip.label  = unname(tip.label),
+    # node.label = unname(node.label),
+    Nnode      = length(node.label)
+  )
+  class(tree) <- 'phylo'
+
+  tree
+
 }
