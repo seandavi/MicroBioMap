876c6fa68c54b226623bad1f7a8667ec06fe9f8c
Sean Davis <seandavi@gmail.com>
2023-08-30 08:31:56 -0400

    styler according to bioccheck guidelines

--- tests/testthat/test-test_get_coldata.R
+++ tests/testthat/test-test_get_coldata.R
@@ -1,9 +1,11 @@
 test_that("sample metadata download works", {
-  bfc = BiocFileCache::BiocFileCache()
-  coldat = .get_compendium_coldata(bfc)
-  expect_equal(ncol(coldat), 11)
-  expect_contains(colnames(coldat), c("srs","project","srr","library_strategy",
-                                      "library_source","pubdate","total_bases",
-                                      "instrument","geo_loc_name",
-                                      "region"))
+    bfc <- BiocFileCache::BiocFileCache()
+    coldat <- .get_compendium_coldata(bfc)
+    expect_equal(ncol(coldat), 11)
+    expect_contains(colnames(coldat), c(
+        "srs", "project", "srr", "library_strategy",
+        "library_source", "pubdate", "total_bases",
+        "instrument", "geo_loc_name",
+        "region"
+    ))
 })
