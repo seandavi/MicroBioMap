f25043b588c914c4791d75b716b1aea0b792451c
Sean Davis <seandavi@gmail.com>
2023-09-08 10:16:15 -0400

    Fix tests after function name changes

--- tests/testthat/test-test_get_coldata.R
+++ tests/testthat/test-test_get_coldata.R
@@ -1,6 +1,6 @@
 test_that("sample metadata download works", {
     bfc <- BiocFileCache::BiocFileCache()
-    coldat <- .get_compendium_coldata(bfc)
+    coldat <- .getCompendiumColdata(bfc)
     expect_equal(ncol(coldat), 11)
     expect_contains(colnames(coldat), c(
         "srs", "project", "srr", "library_strategy",
--- tests/testthat/test-test_get_compendium.R
+++ tests/testthat/test-test_get_compendium.R
@@ -1,6 +1,6 @@
 test_that("getting compendium works as expected", {
     skip_on_ci()
-    cpd <- get_compendium()
+    cpd <- getCompendium()
     expect_s4_class(cpd, "TreeSummarizedExperiment")
     expect_gt(nrow(cpd), 1000)
     expect_gt(ncol(cpd), 1000)
